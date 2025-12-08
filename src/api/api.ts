6addc7c5990c805343e4c72ce0a6d2b8fd2902a8
pali-s <palishma.0812@gmail.com>
2025-12-08 23:08:13 +0545

    .env changes

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { queryClient } from "../main";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: "/.netlify/functions/api"
 })
 
 //set bearer token
